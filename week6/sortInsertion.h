d00914dbf7f7ee2be55067fd1bf481dbb974fc29
Ezaren <ezaren@gmail.com>
2017-05-26 18:52:20 -0600

    Add files via upload

--- week6/sortInsertion.h
+++ week6/sortInsertion.h
@@ -11,15 +11,50 @@
 #ifndef INSERTION_SORT_H
 #define INSERTION_SORT_H
 
+#include <iostream>
 #include "node.h"
 
 /***********************************************
- * INSERTION SORT
- * Sort the items in the array
- **********************************************/
+* INSERTION SORT
+* Sort the items in the array
+**********************************************/
 template <class T>
 void sortInsertion(T array[], int num)
 {
+  Node<T>* n = new Node<T>(array[0]);
+  Node<T>* l = n; //loop node
+  
+  cout << ""; //oddly enough only runs when cout is called.
+  
+  //copy items over
+  for (int i = 1; i < num; i++)
+  {
+    for (Node<T>* l = n; l; l = l->pNext)
+    {
+      if (l->pNext == NULL)
+      {
+        if (array[i] < l->data)
+          insert(array[i], n, true);
+        else
+          insert(array[i], l);
+          
+        break;
+      }
+      else if (array[i] > l->data && array[i] < l->pNext->data)
+      {
+        insert(array[i], l);
+        break;
+      }
+    }
+  }
+  
+  int i = 0;
+  
+  for (Node<T> * l = n; l; l = l->pNext)
+  {
+    array[i] = l->data;
+    i++;
+  }
 }
 
 #endif // INSERTION_SORT_H
