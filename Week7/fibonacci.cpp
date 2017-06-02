23555891c804c8f9a8ea43ac4641dfe7ec58af7d
wesleygdike <wesley.g.dike@gmail.com>
2017-06-01 19:42:40 -0600

    completed Fibo using ints
    the Data Type of Bignumber needs to be written and the Fibonacci methods need to be updated

--- Week7/fibonacci.cpp
+++ Week7/fibonacci.cpp
@@ -19,6 +19,35 @@ using namespace std;
  * The interactive function allowing the user to
  * display Fibonacci numbers
  ***********************************************/
+
+List<int> f(int num)
+{
+	List<int> nums = List<int>();
+	for (int i = 0; i < num; i++)
+	{
+		//push a new number into the list that is equal to the previous 2 numbers
+		if (nums.size() < 2)
+			nums.push_back(1);
+		else
+		{
+			int x = nums.tail->data;
+			int y = nums.tail->pPrev->data;
+			nums.push_back(x + y);
+		}
+	}
+	return nums;
+}
+
+void displayF(int num)
+{
+	List<int> nums = f(num);
+
+	for (ListIterator<int> it = nums.begin(); it != nums.end(); it++)
+	{
+		cout << "\t" << it.p->data << "\n";
+	}
+}
+
 void fibonacci()
 {
    // show the first serveral Fibonacci numbers
@@ -27,12 +56,14 @@ void fibonacci()
    cin  >> number;
 
    // your code to display the first <number> Fibonacci numbers
+   displayF(number);
 
    // prompt for a single large Fibonacci
    cout << "Which Fibonacci number would you like to display? ";
    cin  >> number;
 
    // your code to display the <number>th Fibonacci number
+   cout << "\t" << f(number).tail->data << "\n";
 }
 
 
