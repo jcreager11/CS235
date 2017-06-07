6b679d2e3e416d98fc3e7921f91e92ff928ea34d
Ezaren <ezaren@gmail.com>
2017-06-06 22:09:24 -0600

    Add files via upload

---
+++ week8/huffman.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include "huffman.h"
+using namespace std;
+
+void huffman()
+{
+
+}
+
+void huffman(string & filename)
+{
+
+}
---
+++ week8/huffman.h
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+void huffman();
+void huffman(string &);
+
+#endif
